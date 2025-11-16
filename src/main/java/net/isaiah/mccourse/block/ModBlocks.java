51cf597c51521dddb438ec187c1c535b48364ca9
Mares <maresi3413@gmail.com>
2025-11-15 22:13:50 -0800

    Files

--- src/main/java/net/isaiah/mccourse/block/ModBlocks.java
+++ src/main/java/net/isaiah/mccourse/block/ModBlocks.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.isaiah.mccourse.MCCourseMod;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -13,6 +14,7 @@ import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.Registries;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 import java.util.function.Function;
 
@@ -52,10 +54,50 @@ public class ModBlocks {
     public static final Block FLUORITE_BLOCK = register(
             "fluorite_block",
             Block::new,
-            AbstractBlock.Settings.create()
-                    .sounds(BlockSoundGroup.AMETHYST_BLOCK)
-                    .strength(4.0F)
-                    .requiresTool(),
+            AbstractBlock.Settings.create().sounds(BlockSoundGroup.AMETHYST_BLOCK)
+                    .strength(4.0F).requiresTool(),
+            true
+    );
+
+    public static final Block FLUORITE_ORE = register(
+            "fluorite_ore",
+            settings -> new ExperienceDroppingBlock(
+                    UniformIntProvider.create(2, 4),
+                    settings
+            ),
+            AbstractBlock.Settings.create().strength(4.0F).requiresTool(),
+            true
+    );
+
+    public static final Block FLUORITE_DEEPSLATE_ORE = register(
+            "fluorite_deepslate_ore",
+            settings -> new ExperienceDroppingBlock(
+                    UniformIntProvider.create(3, 6),
+                    settings
+            ),
+            AbstractBlock.Settings.create().sounds(BlockSoundGroup.DEEPSLATE)
+                    .strength(6F).requiresTool(),
+            true
+    );
+
+    public static final Block FLUORITE_END_ORE = register(
+            "fluorite_end_ore",
+            settings -> new ExperienceDroppingBlock(
+                    UniformIntProvider.create(3, 6),
+                    settings
+            ),
+            AbstractBlock.Settings.create().strength(6F).requiresTool(),
+            true
+    );
+
+    public static final Block FLUORITE_NETHER_ORE = register(
+            "fluorite_nether_ore",
+            settings -> new ExperienceDroppingBlock(
+                    UniformIntProvider.create(3, 6),
+                    settings
+            ),
+            AbstractBlock.Settings.create().sounds(BlockSoundGroup.NETHER_ORE)
+                    .strength(6F).requiresTool(),
             true
     );
 
--- src/main/java/net/isaiah/mccourse/item/ModItemGroups.java
+++ src/main/java/net/isaiah/mccourse/item/ModItemGroups.java
@@ -27,6 +27,10 @@ public class ModItemGroups {
             FabricItemGroup.builder().displayName(Text.translatable("itemgroup.fluorite_blocks"))
                     .icon(() -> new ItemStack(ModBlocks.FLUORITE_BLOCK)).entries((displayContext, entries) -> {
                         entries.add(ModBlocks.FLUORITE_BLOCK);
+                        entries.add(ModBlocks.FLUORITE_ORE);
+                        entries.add(ModBlocks.FLUORITE_DEEPSLATE_ORE);
+                        entries.add(ModBlocks.FLUORITE_END_ORE);
+                        entries.add(ModBlocks.FLUORITE_NETHER_ORE);
 
 
                     }).build());
